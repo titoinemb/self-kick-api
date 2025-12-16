781316f059735db5d3c73a975abbeea99b32b5a4
titoinemb <mb_titoine@proton.me>
2025-12-16 13:09:42 -0500

    feat(api): get channel info

---
+++ lib/api/channel.rb
@@ -0,0 +1,20 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
+def channel(name)
+  uri = URI.parse("https://kick.com/api/v1/channels/#{name}")
+  response = Net::HTTP.get_response(uri)
+
+  if response.is_a?(Net::HTTPSuccess)
+    data = JSON.parse(response.body)
+    puts data
+  else
+    puts "Erreur : #{response.message}"
+  end
+end
+
+
+# Exemple d'utilisation
+
+channel("akaruu")
